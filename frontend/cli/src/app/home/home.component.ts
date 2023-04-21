8db27ed7092409edbd47b8667d63b4a96431de1d
PACHECO, SOFIA AILIN <sofiailin10@gmail.com>
2023-04-20 22:37:37 -0300

    personas

--- frontend/cli/src/app/home/home.component.ts
+++ frontend/cli/src/app/home/home.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   template: `
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-  <h1 class="display-4">Bienvenido a Print the Bill</h1>
-  <p class="lead">Este es el front end de nuestra aplicación.</p>
+  <h1 class="display-4">Bienvenido a Partes Docentes</h1>
+  <p class="lead">Este es el front end de la aplicación partes docentes.</p>
   </div>
   `,
   styles: [
